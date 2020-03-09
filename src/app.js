14ef67cd6098e62f37fef59ae5a131f5fc82facf
MrPeaceDuke <daemonic120@yandex.ru>
2020-03-09 17:52:39 +0500

    stream

--- src/app.js
+++ src/app.js
@@ -2,4 +2,6 @@ const openStream = require('./openStream');
 
 openStream();
 
-console.log('App js true');
+
+
+// openStream();
--- src/openStream.js
+++ src/openStream.js
@@ -1,11 +1,24 @@
+const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
+const $ = require('jquery');
+
 function openStream(){
     navigator.mediaDevices.getUserMedia({ audio:false, video:true })
     .then(stream => {
-        const video = document.getElementById('localStream');
-        video.srcObject = stream;
-        video.onloadedmetadata = function() {
-            video.play();
-        }
+        playVideo(stream, 'localStream');
+
+        const p = new Peer({ initiator: location.hash === '#1', trickle: false, stream });
+
+        p.on('signal', token => {
+            $('#tokenArea').val(JSON.stringify(token))
+        });
+
+        $('#btnConnect').click(() => {
+            const friendSignal = JSON.parse($('#tokenYourFriend').val());
+            p.signal(friendSignal);
+        });
+
+        p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
     })
     .catch(err => console.log(err))
 }
