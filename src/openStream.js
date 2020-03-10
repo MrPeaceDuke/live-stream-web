958bbaf49fa318823d284ed347bcfda108d6a9ad
MrPeaceDuke <daemonic120@yandex.ru>
2020-03-10 09:50:25 +0500

    package fix

--- src/openStream.js
+++ src/openStream.js
@@ -26,22 +26,7 @@ function openStream(){
         {urls:'stun:stun.voipbuster.com'},
         {urls:'stun:stun.voipstunt.com'},
         {urls:'stun:stun.voxgratia.org'},
-        {urls:'stun:stun.xten.com'},
-        {
-            urls: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }]}] },
+        {urls:'stun:stun.xten.com'}]}] },
         trickle: false, 
         stream });
 
